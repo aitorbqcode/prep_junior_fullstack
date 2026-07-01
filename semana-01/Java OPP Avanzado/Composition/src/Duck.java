3de15604631ae5667cc7c98f3b3071bb696d488d
Aitor <aitorbqcode@gmail.com>
2026-07-01 12:28:25 +0200

    feat: composition example

---
+++ semana-01/Java OPP Avanzado/Composition/src/Duck.java
@@ -0,0 +1,15 @@
+public class Duck{
+
+    /* Var */
+    private SwimmingBehavior swimmingBehavior = new SwimmingBehavior();
+
+    /* Getter & Setter */
+    public SwimmingBehavior getSwimmingBehavior() { return swimmingBehavior; }
+
+    public void setSwimmingBehavior(SwimmingBehavior swimmingBehavior) { this.swimmingBehavior = swimmingBehavior; }
+
+    /* Methods */
+    public void swim() {
+        swimmingBehavior.swim();
+    }
+}
---
+++ semana-01/Java OPP Avanzado/Composition/src/SwimmingBehavior.java
@@ -0,0 +1,5 @@
+public class SwimmingBehavior {
+
+    /* Methods */
+    public void swim(){ System.out.println("The animal is swimming"); }
+}
